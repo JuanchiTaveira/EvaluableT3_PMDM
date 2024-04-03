7ff71668dd787e041cbc10446cc0bc8f9fd786cc
JuanchiTaveira <juantaveira10@gmail.com>
2024-04-03 13:26:11 +0200

    do login and add TODO

--- app/src/main/java/com/example/evaluablet3/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/evaluablet3/fragments/FirstFragment.kt
@@ -1,13 +1,20 @@
 package com.example.evaluablet3.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.navigation.fragment.findNavController
 import com.example.evaluablet3.R
 import com.example.evaluablet3.databinding.FragmentFirstBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.FirebaseDatabase
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,6 +26,7 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,9 +41,28 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*binding.buttonFirst.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
+
+            // oculto el teclado
+            val inputMethodManager = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+            val email = binding.editTextUser.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        //TODO: move to menuActivity
+                    } else {
+                        Snackbar.make(binding.root, "Usuario o contraseña incorrectos", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }*/
+        }
     }
 
     override fun onDestroyView() {
