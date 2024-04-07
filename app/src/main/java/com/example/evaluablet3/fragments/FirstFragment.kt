e1854f11c9f5992842c46540fde177860730d031
JuanchiTaveira <juantaveira10@gmail.com>
2024-04-07 19:45:05 +0200

    add request call to fill leagueList

--- app/src/main/java/com/example/evaluablet3/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/evaluablet3/fragments/FirstFragment.kt
@@ -7,8 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.evaluablet3.adapters.LeaguesAdapter
 import com.example.evaluablet3.databinding.FragmentFirstBinding
+import org.json.JSONObject
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -20,30 +24,49 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var leaguesList: ArrayList<String> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
-
-        val leaguesList: List<String> = listOf("Premier League", "Bundesliga", "Eredivise")
-
-        binding.recyclerLeagues.adapter = LeaguesAdapter(leaguesList, requireActivity())
-        binding.recyclerLeagues.layoutManager = LinearLayoutManager(requireActivity().applicationContext, RecyclerView.VERTICAL, false)
-
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fillLeaguesList()
+
+        binding.recyclerLeagues.adapter = LeaguesAdapter(leaguesList, requireActivity())
+        binding.recyclerLeagues.layoutManager = LinearLayoutManager(requireActivity().applicationContext, RecyclerView.VERTICAL, false)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun fillLeaguesList() {
+
+        val leaguesRequest = JsonObjectRequest(Request.Method.GET,
+            "https://www.thesportsdb.com/api/v1/json/3/all_leagues.php",
+            null,
+            { response ->
+                val jsonArray = response.getJSONArray("leagues")
+
+                for (i in 0 until jsonArray.length()) {
+                    val actualLeague = jsonArray[i] as JSONObject
+                    if (actualLeague.getString("strSport").equals("Soccer")) {
+                        leaguesList.add(actualLeague.getString("strLeague"))
+                    }
+                }
+
+                binding.recyclerLeagues.adapter?.notifyDataSetChanged()
+            },
+            null)
+
+        Volley.newRequestQueue(requireActivity().applicationContext).add(leaguesRequest)
     }
 }
