d07fb87acd846b8f05ce564e361c785889266fcd
JuanchiTaveira <juantaveira10@gmail.com>
2024-04-09 00:26:15 +0200

    change star colour if team is on favs

--- app/src/main/java/com/example/evaluablet3/adapters/TeamsAdapter.kt
+++ app/src/main/java/com/example/evaluablet3/adapters/TeamsAdapter.kt
@@ -11,10 +11,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.evaluablet3.R
 import com.example.evaluablet3.model.Team
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class TeamsAdapter(var teamsList: List<Team>, var context: Context) : RecyclerView.Adapter<TeamsAdapter.MyHolder>() {
 
     val listener: OnClickTeamListener = context as OnClickTeamListener
+    val database = FirebaseDatabase.getInstance("https://evaluablet3-pmdm-default-rtdb.europe-west1.firebasedatabase.app/")
+    val auth = FirebaseAuth.getInstance()
 
     class MyHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         var teamName: TextView
@@ -39,8 +46,19 @@ class TeamsAdapter(var teamsList: List<Team>, var context: Context) : RecyclerVi
         holder.teamName.text = actualTeam.name
         Glide.with(context).load(actualTeam.image).into(holder.teamImg)
 
+        database.getReference("users/${auth.currentUser!!.uid}/favorites").child(actualTeam.idTeam).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    holder.btnAddToFavs.setImageResource(android.R.drawable.btn_star_big_on)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
         holder.btnAddToFavs.setOnClickListener {
             listener.addTeamToFavs(actualTeam)
+            holder.btnAddToFavs.setImageResource(android.R.drawable.btn_star_big_on)
         }
     }
 
